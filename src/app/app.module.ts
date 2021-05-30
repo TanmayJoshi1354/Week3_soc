803c924c1b77134b4bb0e2b5ded8e7b9b740885d
TanmayJoshi1354 <200070027@iitb.ac.in>
2021-05-31 04:38:01 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { UploadPageComponent } from './upload-page/upload-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
